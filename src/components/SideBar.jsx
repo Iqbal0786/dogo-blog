f94642ebc810ac6053364f7acdb895738e590107
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-14 17:19:48 +0530

    added SideNavbar component

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function () {
+  return (
+   <>
+     <div className='navbar'>
+        <h1>The Dogo Blogs</h1>
+          <div className="links">
+            <a href="/">Home</a>
+            <a href="/create">New Blog</a>
+          </div>
+        </div>
+   </>
+  )
+}
