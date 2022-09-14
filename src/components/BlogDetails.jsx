fbf6c73301b1f42ad0850535cfba8af6ead49a90
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-14 19:42:58 +0530

    added route for blog details component

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function BlogDetails() {
+    const {id} = useParams()
   return (
-    <div className='blog-details'>BlogDetails</div>
+    <div className='blog-details'>BlogDetails {id}</div>
   )
 }
--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function 
 ({blogs , title ,deleteBlogHandler}) {
@@ -7,8 +8,10 @@ export default function
         <h2>{title}</h2>
 {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
+          <Link to={`blogs/${blog.id}`}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
+          </Link>
           <button onClick={()=>{
              deleteBlogHandler(blog.id)
           }}>Delete Blog</button>
