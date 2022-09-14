a561ee6686c2399659193bd6ba7ede3f9c088201
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-14 18:40:53 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 export default function Home() {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState([]);
 
   const deleteBlogHandler=(id)=>{
     setBlogs(blogs.filter(e=>e.id!=id))
 
   }
+
+  useEffect(()=>{
+      fetch("http://localhost:8000/blogs").then((req)=> req.json()).then((res)=>{
+       
+        setBlogs(res)
+      }).catch((error)=>{
+        console.log(error.message)
+      })
+  },[])
   return (
     <div className="home">
-      <BlogList blogs={blogs} title={"All Blogs"} deleteBlogHandler={deleteBlogHandler}/>
+     { blogs && <BlogList blogs={blogs} title={"All Blogs"} deleteBlogHandler={deleteBlogHandler}/>}
     </div>
   );
 }
