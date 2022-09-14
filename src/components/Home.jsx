83c99af3abea584a3af94576b1b7dda781d8b625
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-14 18:46:16 +0530

    added useFetch customHook

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,20 @@
 import React, { useEffect, useState } from "react";
+import useFetch from "../customHooks/useFetch";
 import BlogList from "./BlogList";
 
 export default function Home() {
-  const [blogs, setBlogs] = useState([]);
+  const {data:blogs,isPending,error} =useFetch("http://localhost:8000/blogs")
 
   const deleteBlogHandler=(id)=>{
-    setBlogs(blogs.filter(e=>e.id!=id))
+    //setBlogs(blogs.filter(e=>e.id!=id))
 
   }
 
-  useEffect(()=>{
-      fetch("http://localhost:8000/blogs").then((req)=> req.json()).then((res)=>{
-       
-        setBlogs(res)
-      }).catch((error)=>{
-        console.log(error.message)
-      })
-  },[])
+
   return (
     <div className="home">
+         { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
      { blogs && <BlogList blogs={blogs} title={"All Blogs"} deleteBlogHandler={deleteBlogHandler}/>}
     </div>
   );
