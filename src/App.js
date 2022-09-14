b20a9cdfe8edfc57ad00a9faf6cc8936043e4a37
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-14 17:22:22 +0530

    added home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import SideBar from './components/SideBar';
+import Home from './components/Home';
 
 function App() {
   return (
+    <>
     <div className="App">
        <SideBar/>
+        <div className="content">
+          <Home/>
+        </div>
     </div>
+    </>
   );
 }
 
