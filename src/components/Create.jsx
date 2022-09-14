207babf1c2958906de649403712e4753bd54b8d0
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-14 20:25:10 +0530

    added create post functionality

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Create() {
+    const [input,setInput] =useState({
+        title:"",
+        body:"",
+        author:""
+    })
+    
+    const inputHandler=(e)=>{
+        const {name,value}= e.target
+       setInput({...input ,[name]:value})
+    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+
+    fetch('http://localhost:8000/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(input)
+    }).then(() => {
+      alert('new blog added');
+    })
+  }
   return (
-    <div>Create New Blogs</div>
+    <div
+    className='create'
+    >
+        <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input 
+          type="text" 
+          required 
+          value={input.title}
+          name='title'
+          onChange={inputHandler}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={input.body}
+          name='body'
+          onChange={inputHandler}        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={input.author}
+          onChange={inputHandler}
+          name='author'
+        >
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
   )
 }
